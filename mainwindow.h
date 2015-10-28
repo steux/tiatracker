42792319a6907519c5071920421e35960c3a05a0
Andre Wichmann <andre.wichmann@gmx.de>
2015-10-28 11:55:57 +0100

    added some Solarized colors

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    QColor dark{"#002b36"};
+    QColor darkHighlighted{"#073642"};
+    QColor light{"#fdf6e3"};
+    QColor lightHighlighted{"#eee8d5"};
+    QColor contentDark{"#657b83"};
+    QColor contenLight{"#839496"};
+    QColor red{"#dc322f"};
+
+
+
 private:
     Ui::MainWindow *ui;
 };
