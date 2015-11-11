f4c8574c1ca1d42a121933985a050441adc1cfbe
Andre Wichmann <andre.wichmann@gmx.de>
2015-11-11 14:45:39 +0100

    load and set stylefile

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
+#include <QString>
 
 #include "tiasound/tiasound.h"
 #include "pianokeyboard.h"
@@ -7,9 +9,18 @@
 
 #include <iostream>
 
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Load and set stylesheet
+    QFile styleFile(":/style.qss");
+    styleFile.open(QFile::ReadOnly);
+    a.setStyleSheet(styleFile.readAll());
+
+    // GUI
     MainWindow w;
 
     PianoKeyboard *pk = w.findChild<PianoKeyboard *>("pianoKeyboard");
