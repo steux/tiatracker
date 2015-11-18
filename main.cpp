a44d169edb97bf0b7ae167f411bc6ec0d34bbc99
Andre Wichmann <andre.wichmann@gmx.de>
2015-11-18 15:19:52 +0100

    - QLabel style - QPushButton style - Waveform combobox strings

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <QFile>
 #include <QString>
+#include <QComboBox>
 
 #include "tiasound/tiasound.h"
 #include "pianokeyboard.h"
@@ -23,8 +24,25 @@ int main(int argc, char *argv[])
     // GUI
     MainWindow w;
 
+    /* Initialize GUI elements */
+    // PianoKeyboard starting PitchGuide
     PianoKeyboard *pk = w.findChild<PianoKeyboard *>("pianoKeyboard");
     pk->setInstrumentPitchGuide(TiaSound::perfectPalDist1);
+
+    // Instrument waveforms
+    QComboBox *cbWaveforms = w.findChild<QComboBox *>("comboBoxWaveforms");
+    cbWaveforms->addItems({
+                              TiaSound::getDistorionName(TiaSound::Distortion::BUZZY),
+                              TiaSound::getDistorionName(TiaSound::Distortion::BUZZY_RUMBLE),
+                              TiaSound::getDistorionName(TiaSound::Distortion::FLANGY_WAVERING),
+                              TiaSound::getDistorionName(TiaSound::Distortion::PURE_HIGH),
+                              TiaSound::getDistorionName(TiaSound::Distortion::PURE_BUZZY),
+                              TiaSound::getDistorionName(TiaSound::Distortion::REEDY_RUMBLE),
+                              TiaSound::getDistorionName(TiaSound::Distortion::WHITE_NOISE),
+                              TiaSound::getDistorionName(TiaSound::Distortion::PURE_LOW),
+                              TiaSound::getDistorionName(TiaSound::Distortion::ELECTRONIC_LOW),
+                              TiaSound::getDistorionName(TiaSound::Distortion::ELECTRONIC_HIGH)
+                          });
 
     w.show();
 
