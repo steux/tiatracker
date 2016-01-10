f70182365baec2c0a7c9096942336820e7d0d636
Andre Wichmann <andre.wichmann@gmx.de>
2016-01-10 17:08:34 +0100

    ButtonGroup

--- tracktab.cpp
+++ tracktab.cpp
@@ -1,6 +1,7 @@
 #include "tracktab.h"
 #include <QPushButton>
 #include <QLayout>
+#include <QButtonGroup>
 #include <iostream>
 
 TrackTab::TrackTab(QWidget *parent) : QWidget(parent)
@@ -18,13 +19,23 @@ void TrackTab::registerTrack(Track::Track *newTrack) {
 
 void TrackTab::initTrackTab() {
     QLayout *insLayout = new QVBoxLayout;
+    QButtonGroup *group = new QButtonGroup();
+
+    int id = 0;
     foreach (Track::Instrument ins, pTrack->instruments) {
         QPushButton *insButton = new QPushButton(ins.name, this);
+        insButton->setCheckable(true);
+        if (id == 0) {
+            insButton->setChecked(true);
+        }
         insLayout->addWidget(insButton);
+        group->addButton(insButton, id++);
     }
     foreach (Track::Percussion perc, pTrack->percussion) {
         QPushButton *percButton = new QPushButton(perc.name, this);
+        percButton->setCheckable(true);
         insLayout->addWidget(percButton);
+        group->addButton(percButton, id++);
     }
     QWidget *insSelector = findChild<QWidget *>("trackInstrumentSelector");
     insSelector->setLayout(insLayout);
