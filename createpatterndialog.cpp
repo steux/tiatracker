658b8a6b39963e689f6966df1b91f5fb7db399b4
Andre Wichmann <andre.wichmann@gmx.de>
2016-03-14 17:07:07 +0100

    added GPL2 license boiler plate

--- createpatterndialog.cpp
+++ createpatterndialog.cpp
@@ -1,3 +1,10 @@
+/* TIATracker, (c) 2016 Andre "Kylearan" Wichmann.
+ * Website: https://bitbucket.org/kylearan/tiatracker
+ * Email: andre.wichmann@gmx.de
+ * See the file "license.txt" for information on usage and redistribution
+ * of this file.
+ */
+
 #include "createpatterndialog.h"
 #include "ui_createpatterndialog.h"
 #include "track/track.h"
--- setfrequencydialog.cpp
+++ setfrequencydialog.cpp
@@ -1,3 +1,10 @@
+/* TIATracker, (c) 2016 Andre "Kylearan" Wichmann.
+ * Website: https://bitbucket.org/kylearan/tiatracker
+ * Email: andre.wichmann@gmx.de
+ * See the file "license.txt" for information on usage and redistribution
+ * of this file.
+ */
+
 #include "setfrequencydialog.h"
 #include "ui_setfrequencydialog.h"
 
