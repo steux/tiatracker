4cb85c0f626091d62fcb9c78cb89892420ef8ab7
Andre Wichmann <andre.wichmann@gmx.de>
2016-04-27 10:59:22 +0200

    widgets named, create guide routine started

--- createguidedialog.cpp
+++ createguidedialog.cpp
@@ -1,6 +1,11 @@
 #include "createguidedialog.h"
 #include "ui_createguidedialog.h"
 
+#include <QMap>
+#include <QString>
+#include "tiasound/tiasound.h"
+
+
 CreateGuideDialog::CreateGuideDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CreateGuideDialog)
@@ -11,4 +16,26 @@ CreateGuideDialog::CreateGuideDialog(QWidget *parent) :
 CreateGuideDialog::~CreateGuideDialog()
 {
     delete ui;
+}
+
+/*************************************************************************/
+
+void CreateGuideDialog::on_pushButton_clicked() {
+    QMap<TiaSound::Distortion, QString> checkBoxNames{
+        {TiaSound::Distortion::BUZZY, "checkBoxGuideBuzzy"},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::},
+        {TiaSound::Distortion::}
+    };
+
+    int maxDeviation = ui->spinBoxGuideMaxDeviation->value();
+    int maxOffTune = ui->spinBoxGuideMaxOffTune->value();
+
 }
--- createguidedialog.h
+++ createguidedialog.h
@@ -15,6 +15,9 @@ public:
     explicit CreateGuideDialog(QWidget *parent = 0);
     ~CreateGuideDialog();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::CreateGuideDialog *ui;
 };
