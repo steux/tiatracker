b951d4df79f5ea03e495d2eacb8da8c9cc683ec8
Andre Wichmann <andre.wichmann@gmx.de>
2016-05-04 17:18:17 +0200

    first steps for create guide button click

--- createguidedialog.cpp
+++ createguidedialog.cpp
@@ -4,6 +4,7 @@
 #include <QMap>
 #include <QString>
 #include "tiasound/tiasound.h"
+#include <iostream>
 
 
 CreateGuideDialog::CreateGuideDialog(QWidget *parent) :
@@ -21,21 +22,29 @@ CreateGuideDialog::~CreateGuideDialog()
 /*************************************************************************/
 
 void CreateGuideDialog::on_pushButton_clicked() {
+}
+
+/*************************************************************************/
+
+void CreateGuideDialog::on_pushButtonCreateGuide_clicked() {
     QMap<TiaSound::Distortion, QString> checkBoxNames{
         {TiaSound::Distortion::BUZZY, "checkBoxGuideBuzzy"},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::},
-        {TiaSound::Distortion::}
+        {TiaSound::Distortion::BUZZY_RUMBLE, "checkBoxGuideBuzzyRumble"},
+        {TiaSound::Distortion::FLANGY_WAVERING, "checkBoxGuideFlangyWavering"},
+        {TiaSound::Distortion::PURE_HIGH, "checkBoxGuidePureHigh"},
+        {TiaSound::Distortion::PURE_BUZZY, "checkBoxGuidePureBuzzy"},
+        {TiaSound::Distortion::REEDY_RUMBLE, "checkBoxGuideReedyRumble"},
+        {TiaSound::Distortion::WHITE_NOISE, "checkBoxGuideWhiteNoise"},
+        {TiaSound::Distortion::PURE_LOW, "checkBoxGuidePureLow"},
+        {TiaSound::Distortion::ELECTRONIC_RUMBLE, "checkBoxGuideElectronicRumble"},
+        {TiaSound::Distortion::ELECTRONIC_SQUEAL, "checkBoxGuideElectronicSqueal"}
     };
 
     int maxDeviation = ui->spinBoxGuideMaxDeviation->value();
     int maxOffTune = ui->spinBoxGuideMaxOffTune->value();
 
+    for (int i = 0; i < checkBoxNames.size(); ++i) {
+        TiaSound::Distortion dist = checkBoxNames.keys()[i];
+        QCheckBox *cbDist = findChild<QCheckBox *>(checkBoxNames[dist]);
+    }
 }
